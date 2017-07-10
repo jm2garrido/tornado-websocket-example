de21483afb6146dbb57aa808acae0f530c5d371e
Josemi Garrido <jose.garridog@atos.net>
2017-07-10 10:39:07 +0200

    Changed to add "protected" directory

--- app.py
+++ app.py
@@ -13,10 +13,12 @@ class SocketHandler(websocket.WebSocketHandler):
 
     def open(self):
         if self not in cl:
+            print(self)
             cl.append(self)
 
     def on_close(self):
         if self in cl:
+            print("Cerrado"+str(self))
             cl.remove(self)
 
 class ApiHandler(web.RequestHandler):
@@ -26,6 +28,7 @@ class ApiHandler(web.RequestHandler):
         self.finish()
         id = self.get_argument("id")
         value = self.get_argument("value")
+        print("{} {}".format(id,value))
         data = {"id": id, "value" : value}
         data = json.dumps(data)
         for c in cl:
@@ -35,14 +38,27 @@ class ApiHandler(web.RequestHandler):
     def post(self):
         pass
 
+class ProtectedTest(web.RequestHandler):
+    def get(self):
+        print("Entering protected...")
+        argTest = self.get_argument("test","None")
+        print(self.request.headers)
+        print(argTest)
+        #print(self.get_cookie("loginToken"))
+        self.finish()
+
+
+
 app = web.Application([
     (r'/', IndexHandler),
     (r'/ws', SocketHandler),
     (r'/api', ApiHandler),
+    (r'/protected',ProtectedTest),
     (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
     (r'/(rest_api_example.png)', web.StaticFileHandler, {'path': './'}),
 ])
 
 if __name__ == '__main__':
+    print("Starting...")
     app.listen(8888)
     ioloop.IOLoop.instance().start()
